2ecfb39b27e87ff9d9e6fd87f7085fd01809bc6d
bartoszzak <bartosz.zak0@gmail.com>
2023-07-03 21:00:18 +0200

    logging fix

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import argparse
+import logging
 import os
+import warnings
 
 import openai
-import pandas as pd
 import pm4py
 from dotenv import load_dotenv
 
 from src import xml_parser
 from src.log_analysis import rename_bpmn_objects, determine_object_types, distinguish_tasks_events
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process logs and generate BPMN model')
+    parser.add_argument('input_file', help='Path to the input .xes file')
     parser.add_argument('output_file', help='Path to the output BPMN file')
     return parser.parse_args()
 
@@ -20,24 +28,30 @@ if __name__ == '__main__':
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     args = parse_arguments()
+    logs_path = args.input_file
     enhanced_bpmn_path = args.output_file
 
     # read logs from xes
-    logs = pm4py.read_xes('data/logs/BPI_Challenge 2017.xes')
-    logs.to_pickle('data/pickled_logs/BPI_Challenge 2017.pickle')
-    logs: pd.DataFrame = pd.read_pickle('data/pickled_logs/log.pickle')
+    logger.info("Reading .xes logs...")
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        logs = pm4py.read_xes(os.path.abspath(logs_path))
 
     # generate initial process model
+    logger.info("Discovering model...")
     process_model = pm4py.discover_bpmn_inductive(logs)
     pm4py.write_bpmn(process_model, 'data/model')
     process_model = pm4py.read_bpmn('data/model.bpmn')
+    logger.info("Model discovery complete")
 
     # rename objects
+    logger.info("Renaming objects...")
     unique_obj_names = logs['concept:name'].unique()
     renamed = rename_bpmn_objects(unique_obj_names)
     new_names = list(renamed.values())
 
     # classify objects
+    logger.info("Classifying objects...")
     classified_objects = distinguish_tasks_events(str(new_names))
     tasks = [obj for obj, obj_type in classified_objects.items() if obj_type.lower() == 'task']
     events = [obj for obj, obj_type in classified_objects.items() if obj_type.lower() == 'event']
@@ -46,5 +60,6 @@ if __name__ == '__main__':
 
     # convert to actual BPMN format names
     object_types = {**task_types, **event_types}
-
+    logger.info("Replacing objects...")
     xml_parser.replace_task_names('data/model.bpmn', enhanced_bpmn_path, renamed, object_types)
+    logger.info(f"Enhanced model saved to {os.path.abspath(enhanced_bpmn_path)}")
--- src/log_analysis.py
+++ src/log_analysis.py
@@ -8,14 +8,9 @@ import pandas as pd
 from src import prompt_generation
 
 logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-logger.addHandler(handler)
-FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-logging.basicConfig(format=FORMAT)
-logger.setLevel(logging.DEBUG)
 
 
-def rename_bpmn_objects(object_names: str) -> dict:
+def rename_bpmn_objects(object_names: str) -> dict[str, str]:
     prompt = prompt_generation.generate_rename_objects_prompt(object_names)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -24,14 +19,14 @@ def rename_bpmn_objects(object_names: str) -> dict:
     )
     response_message = response["choices"][0]["message"]["content"]
     response_message = response_message.replace('\'', '\"')
-    logging.debug(response_message)
+    logger.debug(response_message)
 
     renamed_elements = json.loads(response_message)
 
     return renamed_elements
 
 
-def distinguish_tasks_events(object_names: str) -> dict:
+def distinguish_tasks_events(object_names: str) -> dict[str, str]:
     prompt = prompt_generation.generate_task_events_prompt(object_names)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -41,16 +36,16 @@ def distinguish_tasks_events(object_names: str) -> dict:
 
     response_message = response["choices"][0]["message"]["content"]
     response_message = response_message.replace('\'', '\"')
-    logging.debug(response_message)
+    logger.debug(response_message)
     extracted_dicts = __extract_dicts_from_str(response_message.replace('\'', '\"'))
     if len(extracted_dicts) > 1:
-        logging.warning("More than 1 dict found in output! Taking the first one.")
+        logger.warning("More than 1 dict found in output! Taking the first one.")
     classified_objects = extracted_dicts[0]
 
     return classified_objects
 
 
-def determine_object_types(bpmn_objects: list[str], obj_name: str) -> dict:
+def determine_object_types(bpmn_objects: list[str], obj_name: str) -> dict[str, str]:
     if obj_name == 'task':
         prompt = prompt_generation.generate_task_types_prompt(bpmn_objects)
     elif obj_name == 'event':
@@ -66,49 +61,13 @@ def determine_object_types(bpmn_objects: list[str], obj_name: str) -> dict:
 
     response_message = response["choices"][0]["message"]["content"]
     response_message = response_message.replace('\'', '\"')
-    logging.debug(response_message)
+    logger.debug(response_message)
     extracted_dicts = __extract_dicts_from_str(response_message.replace('\'', '\"'))
     if len(extracted_dicts) > 1:
-        logging.warning("More than 1 dict found in output! Taking the first one.")
+        logger.warning("More than 1 dict found in output! Taking the first one.")
     object_types = extracted_dicts[0]
 
     return object_types
-
-
-def convert_to_bpmn_format(object_types: str) -> dict:
-    prompt = prompt_generation.generate_convert_to_bpmn_type_names_prompt(object_types)
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": f"{prompt}"}],
-        temperature=0,
-    )
-
-    response_message = response["choices"][0]["message"]["content"]
-    response_message = response_message.replace('\'', '\"')
-    logging.debug(response_message)
-    extracted_dicts = __extract_dicts_from_str(response_message.replace('\'', '\"'))
-    if len(extracted_dicts) > 1:
-        logging.warning("More than 1 dict found in output! Taking the first one.")
-    bpmn_object_types = extracted_dicts[0]
-
-    return bpmn_object_types
-
-
-def __get_unique_logs_by_name(logs: pd.DataFrame) -> str:
-    """
-    Get logs with unique concept:name fields.
-    :param logs: pandas DataFrame of logs converted from .xes format.
-    :type logs: pandas.DataFrame
-    :return: list of logs with unique names as str objects
-    :rtype: list[str]
-    """
-
-    unique_logs_str = ''
-    unique_logs: pd.DataFrame = logs.drop_duplicates('concept:name').reset_index()
-    for _, log in unique_logs.iterrows():
-        unique_logs_str += re.sub(' +', '  ', f'{str(log.values)}')  # remove multiple spaces and add to str
-
-    return unique_logs_str
 
 
 def __extract_dicts_from_str(s: str) -> list:
@@ -119,5 +78,5 @@ def __extract_dicts_from_str(s: str) -> list:
         try:
             results.append(json.loads(i))
         except json.JSONDecodeError as e:
-            print(e)
+            logger.error(e)
     return results
